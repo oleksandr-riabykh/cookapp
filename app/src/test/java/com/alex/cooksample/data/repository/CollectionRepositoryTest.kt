5c4cae63b64e3b342a76fea7d4427fd2327ea322
Alex R <alexr@resecurity.com>
2022-01-28 01:09:03 +0200

    Updated tests setup, Use TestCoroutineDispatcher

--- app/src/test/java/com/alex/cooksample/data/repository/CollectionRepositoryTest.kt
+++ app/src/test/java/com/alex/cooksample/data/repository/CollectionRepositoryTest.kt
@@ -3,34 +3,37 @@ package com.alex.cooksample.data.repository
 import com.alex.cooksample.data.local.CollectionDao
 import com.alex.cooksample.data.network.CookService
 import io.mockk.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
+@ObsoleteCoroutinesApi
 class CollectionRepositoryTest {
-    @ObsoleteCoroutinesApi
+
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+    private val testDispatcher = TestCoroutineDispatcher()
 
     private val collectionDao: CollectionDao = mockk(relaxed = true)
     private val networkService: CookService = mockk(relaxed = true)
-    private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO
-
     private lateinit var collectionRepo: CollectionsRepository
 
-    @ObsoleteCoroutinesApi
-    @ExperimentalCoroutinesApi
+
     @Before
     fun setupTest() {
         MockKAnnotations.init(this, relaxUnitFun = true)
         Dispatchers.setMain(mainThreadSurrogate)
-        collectionRepo = CollectionsRepository(collectionDao, networkService, coroutineDispatcher)
+        collectionRepo = CollectionsRepository(collectionDao, networkService, testDispatcher)
     }
 
-    @ObsoleteCoroutinesApi
-    @ExperimentalCoroutinesApi
     @After
     fun tearDown() {
         Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
@@ -39,30 +42,35 @@ class CollectionRepositoryTest {
     }
 
     @Test
-    fun `If no cached collections then get response from server`() = runBlocking {
-        //given
-        coEvery { collectionDao.getAll() } returns listOf()
-        coEvery { networkService.getCollections() } returns mockk(relaxed = true)
+    fun `If no cached collections then get response from server`() {
+        testDispatcher.runBlockingTest {
+            //given
+            coEvery { collectionDao.getAll() } returns listOf()
+            coEvery { networkService.getCollections() } returns mockk(relaxed = true)
 
-        //when
-        collectionRepo.getCollections()
+            //when
+            collectionRepo.getCollections()
 
-        //then
-        coVerify { networkService.getCollections() }
+            //then
+            coVerify { networkService.getCollections() }
+        }
+        testDispatcher.cleanupTestCoroutines()
     }
 
     @Test
-    fun `If collections then get collection by id from server is not empty`() = runBlocking {
+    fun `If collections then get collection by id from server is not empty`() {
+        testDispatcher.runBlockingTest {
+            val collectionId = 2
 
-        val collectionId = 2
+            //given
+            coEvery { networkService.getCollectionById(collectionId) } returns mockk(relaxed = true)
 
-        //given
-        coEvery { networkService.getCollectionById(collectionId) } returns mockk(relaxed = true)
+            //when
+            collectionRepo.getCollectionById(collectionId)
 
-        //when
-        collectionRepo.getCollectionById(collectionId)
-
-        //then
-        coVerify { networkService.getCollectionById(collectionId) }
+            //then
+            coVerify { networkService.getCollectionById(collectionId) }
+        }
+        testDispatcher.cleanupTestCoroutines()
     }
 }
--- app/src/test/java/com/alex/cooksample/data/repository/RecipesRepositoryTest.kt
+++ app/src/test/java/com/alex/cooksample/data/repository/RecipesRepositoryTest.kt
@@ -4,13 +4,18 @@ import com.alex.cooksample.data.network.CookService
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class RecipesRepositoryTest {
-    private val networkService: CookService = mockk(relaxed = true)
 
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    private val networkService: CookService = mockk(relaxed = true)
     private lateinit var recipesRepo: RecipesRepository
 
     @Before
@@ -19,45 +24,53 @@ class RecipesRepositoryTest {
     }
 
     @Test
-    fun `If recipes then get response from server`() = runBlocking {
-        //given
-        coEvery { networkService.getRecipes() } returns mockk(relaxed = true)
+    fun `If recipes then get response from server`() {
+        testDispatcher.runBlockingTest {
+            //given
+            coEvery { networkService.getRecipes() } returns mockk(relaxed = true)
 
-        //when
-        recipesRepo.getRecipes()
+            //when
+            recipesRepo.getRecipes()
 
-        //then
-        coVerify { networkService.getRecipes() }
+            //then
+            coVerify { networkService.getRecipes() }
+        }
+        testDispatcher.cleanupTestCoroutines()
     }
 
 
     @Test
-    fun `If recipes by collection id then get response from server`() = runBlocking {
+    fun `If recipes by collection id then get response from server`() {
+        testDispatcher.runBlockingTest {
+            val collectionId = 2
 
-        val collectionId = 2
+            //given
+            coEvery { networkService.getRecipesByCollectionId(collectionId) } returns mockk(relaxed = true)
 
-        //given
-        coEvery { networkService.getRecipesByCollectionId(collectionId)} returns mockk(relaxed = true)
+            //when
+            recipesRepo.getRecipesByCollectionId(collectionId)
 
-        //when
-        recipesRepo.getRecipesByCollectionId(collectionId)
+            //then
+            coVerify { networkService.getRecipesByCollectionId(collectionId) }
+        }
+        testDispatcher.cleanupTestCoroutines()
 
-        //then
-        coVerify { networkService.getRecipesByCollectionId(collectionId) }
     }
 
     @Test
-    fun `If recipes by id then get response from server`() = runBlocking {
+    fun `If recipes by id then get response from server`() {
+        testDispatcher.runBlockingTest {
+            val recipeId = 2
 
-        val recipeId = 2
+            //given
+            coEvery { networkService.getRecipeById(recipeId) } returns mockk(relaxed = true)
 
-        //given
-        coEvery { networkService.getRecipeById(recipeId)} returns mockk(relaxed = true)
+            //when
+            recipesRepo.getRecipeById(recipeId)
 
-        //when
-        recipesRepo.getRecipeById(recipeId)
-
-        //then
-        coVerify { networkService.getRecipeById(recipeId) }
+            //then
+            coVerify { networkService.getRecipeById(recipeId) }
+        }
+        testDispatcher.cleanupTestCoroutines()
     }
 }
