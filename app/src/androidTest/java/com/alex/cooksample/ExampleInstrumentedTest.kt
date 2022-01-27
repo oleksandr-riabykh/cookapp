cc06b12c66e9651b2fcc0a119ef7fb1b9feec7bc
Alex R <alexr@resecurity.com>
2022-01-27 15:59:26 +0200

    UI functional test setup

--- app/src/androidTest/java/com/alex/cooksample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/alex/cooksample/ExampleInstrumentedTest.kt
@@ -1,23 +1,46 @@
 package com.alex.cooksample
 
 import androidx.test.core.app.launchActivity
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.NoMatchingViewException
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.filters.LargeTest
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+@LargeTest
+class CollectionsFlowTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun testEvent() {
         val scenario = launchActivity<MainActivity>()
+    }
+
+    @Test
+    fun listIsDisplayed() {
+        onView(withId(R.id.root_recycler)).check(matches(isDisplayed()))
+    }
+
+    @Test(expected = NoMatchingViewException::class)
+    fun itemWithText_doesNotExist() {
+        val fakeText = "sgrsgdv dr rg r"
+        onData(withText(fakeText))
+            .onChildView(withId(R.id.collectionTitleTextView))
+            .check(doesNotExist())
+    }
+
+    @Test
+    fun navigateToRecipesPositive() {
+
     }
 }
